83994ca1ce1020a045e1dddad69b2eea284a79ea
Alessio Degani <alessio.degani@gmail.com>
2019-11-01 17:50:50 +0100

    First working version of DX7 to volca translator

--- dx2volca/helpers/dx7parser.py
+++ dx2volca/helpers/dx7parser.py
@@ -3,18 +3,43 @@ from dx2volca.helpers import logger
 log = logger.Logger()
 
 class Dx7Parser(object):
+    """DX7 SYSEX parser and transalte
+
+    This class parses the SYSEX messages sent to DX7 and thranslate the 1 Voice bulk data in order
+    to behave correctly with Korg Volca FM
+    """
     def __init__(self, enable_translation=True):
+        """Class constructor
+        
+        Keyword Arguments:
+            enable_translation {bool} -- Enbla real time SYSEX translation (default: {True})
+        """
         self.enable_translation = enable_translation
         self.sysex_marker = 240
         self.sysex_id = 67
-        pass
+        self.sysex_end = 247
+        return
 
     def translation_enabled(value=True):
+        """Enable/Disable SYSEX transalation
+        
+        Keyword Arguments:
+            value {bool} -- Enable translation if True (default: {True})
+        """
         self.enable_translation = value
 
     def parse(self, msg):
+        """Parse the SYSEX messages
+        
+        Arguments:
+            msg {[list]} -- SYSEX message byte list
+        
+        Returns:
+            [list] -- The translated SYSEX message, if applicable
+        """
         out_msg = list.copy(msg)
 
+        # Check message type and tke different actions
         msg_type = self._get_msg_type(msg)
         if msg_type == 0:
             # 1 Voice Bulk data
@@ -29,8 +54,27 @@ class Dx7Parser(object):
         return out_msg
 
     def _translate_and_print(self, msg):
-        if not self.enable_translation:
+        """If the SYSEX is a 1 voice bulk data, parse it and translate (if enabled)
+        
+        Arguments:
+            msg {[list]} -- SYSEX message byte list
+        
+        Returns:
+            [list] -- The translated SYSEX message, if applicable
+        """
+        if msg[-1] != self.sysex_end:
+            log.warning("Malformed SYSEX (end: %d)" % msg[-1])
             return msg
+        
+        channel = (msg[2] & 15) + 1
+        byte_count = int(msg[4]) * 128 + int(msg[5])
+        voice_name = ''.join(chr(i) for i in msg[150:161])
+        checksum = msg[-2]
+        # THE FIX: all operator in ON
+        # Korg Volca FM interprets the checksum byte as operator ON/OFF command
+        msg[-2] = 63
+        log.info("FIX: ALL operators are ON")
+        log.debug("%s: 1 Voice bulk on ch %d, with %d bytes (CHECK: %s)" % (voice_name, channel, byte_count, checksum))
         return msg
 
     def _print_32voice_data_info(self, msg):
@@ -40,12 +84,20 @@ class Dx7Parser(object):
         pass
 
     def _get_msg_type(self, msg):
+        """Check the type of the DX7 SYSEX message
+        
+        Arguments:
+            msg {[list]} -- SYSEX message byte list
+        
+        Returns:
+            int -- Message type. 0 for 1 Voice bulk, 1 for 32 Voice bulk, 2 for par. change, -1 if not recognized
+        """
 
         if msg[0] != self.sysex_marker:
             log.warning("Message is not a SYSEX (marker %d)" % msg[0])
             return -1
         if msg[1] != self.sysex_id:
-            log.warning("Message is not a SYSEX for Tamaha DX7 (id %d)" % msg[1])
+            log.warning("Message is not a DX7 SYSEX (id %d)" % msg[1])
             return -1
         if len(msg) == 163:
             log.debug("Message is DX7 1 Voice Bulk data")
